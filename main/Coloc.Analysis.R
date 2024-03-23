02137c4a1457bb4185e82377cf3d2589eb5ec44e
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-03-23 23:10:48 +0000

    Update Coloc.Analysis.R

--- main/Coloc.Analysis.R
+++ main/Coloc.Analysis.R
@@ -9,6 +9,7 @@ use.ld <- ifelse(tolower(args[6])=="yes",T,F)
 ld.threshold <- as.numeric(args[7])      ## LD threshold for defining regions (it should be between 0 and 1)
 ref.genome.prefix <- args[8]
 out.pref <- args[9]
+sink(file = paste0(out.pref , ".log.txt"))
 #################################################################
 cat("Input arguments:\n")
 cat("     QTL directory= ",dirname(QTL.file),"\n")
@@ -230,7 +231,8 @@ if(total.common.snps > 0){
   }else{
     warning("There is no shared SNPs between QTLs and all tested regions in GWAS!",call. = F)
   }
-  
+sink()
+
 }else{
   warning("There is no shared SNPs between QTLs and GWAS!",call. = F)
 }
